e405391c34b41cd2235aee455cbda607a4bcd7d7
Darshankumar Radadiya <dradadiya@seattleu.edu>
2021-12-15 14:04:19 -0800

    changes done.

--- TwoSum.py
+++ TwoSum.py
@@ -2,7 +2,7 @@
 # 1. Two Sum (Easy)
 # Author : Darshan Radadiya.
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         hashMap = {}
         for i,n in enumerate(nums):
             diff = target - n
@@ -10,3 +10,5 @@ class Solution:
                 return(hashMap[diff], i)
             hashMap[n] = i
         return 
+sol = Solution()
+print(sol.twoSum([1,2,3,4,5],7))
