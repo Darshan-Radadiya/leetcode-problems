f55259dc4350b559ac57a4e2b65aca99e16a25c0
darshan <darshanradadiya98@gmail.com>
2023-05-13 15:10:57 -0700

    Graph

---
+++ leetcode/133-CloneGraph-M.py
@@ -0,0 +1,75 @@
+# Python program for
+# validation of a graph
+
+# import dictionary for graph
+from collections import defaultdict
+
+# function for adding edge to graph
+graph = defaultdict(list)
+def addEdge(graph,u,v):
+	graph[u].append(v)
+
+# definition of function
+def generate_edges(graph):
+	edges = []
+
+	# for each node in graph
+	for node in graph:
+		
+		# for each neighbour node of a single node
+		for neighbour in graph[node]:
+			
+			# if edge exists then append
+			edges.append([node, neighbour])
+	return edges
+
+# declaration of graph as dictionary
+addEdge(graph,'a','c')
+addEdge(graph,'b','c')
+addEdge(graph,'b','e')
+addEdge(graph,'c','d')
+addEdge(graph,'c','e')
+addEdge(graph,'c','a')
+addEdge(graph,'c','b')
+addEdge(graph,'e','b')
+addEdge(graph,'d','c')
+addEdge(graph,'e','c')
+
+# Driver Function call
+# to print generated graph
+print(generate_edges(graph))
+
+
+
+
+
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        old_to_new = {}
+        
+        def clone(node):
+            if node in old_to_new:
+                return old_to_new[node]
+            
+            copy = Node(node.val)
+            old_to_new[node] = copy
+            
+            for nei in node.neighbors:
+                copy.neighbors.append(clone(nei))
+            return copy 
+        
+        return clone(node) if node else None
+
+node = [[2,4],[1,3],[2,4],[1,3]]
+Output = [[2,4],[1,3],[2,4],[1,3]]
+sol = Solution()
+print("Expected Output:", Output)
+print("Output: ",sol.cloneGraph(node))  
---
+++ leetcode/200-NumberOfIsland-M.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numIslands(self, grid):
+        
+        if not grid:
+            return 
+
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        numberOfIsland = 0
+
+        #recursive 
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r,c) in visited or grid[r][c] == "0":
+                return  
+            visited.add((r,c))
+
+            dfs(r - 1, c)  # Up
+            dfs(r + 1, c)  # Down
+            dfs(r, c - 1)  # Left
+            dfs(r, c + 1)  # Right
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    numberOfIsland += 1
+                    dfs(r,c)
+        return numberOfIsland
+    
+grid = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+Output = 1
+sol = Solution()
+print("Expected Output:", Output)
+print("Output: ",sol.numIslands(grid))
