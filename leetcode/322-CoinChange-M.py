3a91d448d8649d1d1cf5b7eaf96b2358f7052484
darshan <darshanradadiya29@gmail.com>
2023-06-01 23:58:19 -0700

    coin change

---
+++ leetcode/322-CoinChange-M.py
@@ -0,0 +1,20 @@
+class Solution:
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
+sol = Solution()
+coins = [1,2,5]
+amount = 11
+ExpectedOutput = 3
+Output = sol.coinChange(coins,amount) 
+print("\nOutput is:      ", Output ,"\n" )
+print("Expected Output:",ExpectedOutput,"\n" )
+print("The output matches with expected Output: ", ExpectedOutput == Output, "\n" )
+print("Time Complexity is: O(Amount*len(coins))\n" )
