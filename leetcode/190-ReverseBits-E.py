7dcfb17c0fdf262b14db9bf3e4b6c6f16f5a6e9d
darshan <darshanradadiya29@gmail.com>
2023-08-04 00:46:19 -0700

    LC 190 Reverse bit Easy

---
+++ leetcode/190-ReverseBits-E.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            bit = (n >> i) & 1
+            res += (bit << (31 - i))
+        return res
+
+
+
+sol = Solution()
+n = 43261596 # 00000010100101000001111010011100
+ExpectedOutput = 964176192 # 00111001011110000010100101000000
+Output = sol.reverseBits(n) 
+print("\nOutput is:      ", Output ,"\n" )
+print("Expected Output:",ExpectedOutput,"\n" )
+print("The output matches with expected Output: ", ExpectedOutput == Output, "\n" )
+print("Time & Space Complexity is: O(1)\n" )
