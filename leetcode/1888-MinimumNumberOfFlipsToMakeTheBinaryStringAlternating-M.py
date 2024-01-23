c20e521d80008e9be4dcd82c8302888ea2de10b0
darshan <darshanradadiya29@gmail.com>
2024-01-22 20:40:02 -0800

    LC - Sliding Window - 444,950

---
+++ leetcode/1888-MinimumNumberOfFlipsToMakeTheBinaryStringAlternating-M.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minFlips(self, s: str) -> int:
+        n = len(s)
+        s = s + s
+        alt1, alt2 = "", ""
+
+        for i in range(len(s)):
+            alt1 += "0" if i % 2 == 0 else "1"
+            alt2 += "1" if i % 2 == 0 else "0"
+
+        res = float('inf')
+        diff1, diff2 = 0, 0
+        l = 0
+        for r in range(len(s)):
+            if s[r] != alt1[r]:
+                diff1 += 1
+            if s[r] != alt2[r]:
+                diff2 += 1
+            if (r - l + 1) > n:
+                if s[l] != alt1[l]:
+                    diff1 -= 1
+                if s[l] != alt2[l]:
+                    diff2 -= 1
+                l += 1
+            if (r - l + 1) == n:
+                res = min(res, diff1, diff2)
+        return res
+
+s =  "111000"
+ExpectedOutput = 2
+sol = Solution()
+Output = sol.minFlips(s) 
+print("\nOutput is:      ", Output ,"\n" )
+print("Expected Output:",ExpectedOutput,"\n" )
+print("The output matches with expected Output: ", ExpectedOutput == Output, "\n" )
+print("Time Complexity is: O(n)\n" )
