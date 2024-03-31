587c63fde562694a95f40e77f89deceafa4df458
darshan <darshanradadiya29@gmail.com>
2024-03-31 01:00:30 -0700

    LC - Heap - 274,774

---
+++ leetcode/Heaps & Priority Queue/215-KthLargestElementInAnArray-M.py
@@ -0,0 +1,21 @@
+from typing import List
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        maxHeap = []
+        heapq.heapify(maxHeap)
+        for n in nums:
+            heapq.heappush(maxHeap, n)
+            while len(maxHeap) > k:
+                heapq.heappop(maxHeap)
+        return maxHeap[0]
+    
+sol = Solution()
+nums = [3,2,3,1,2,4,5,5,6]
+k = 4
+ExpectedOutput = 4
+Output = sol.findKthLargest(nums, k) 
+print("\nOutput is:      ", Output ,"\n" )
+print("Expected Output:",ExpectedOutput,"\n" )
+print("The output matches with expected Output: ", ExpectedOutput == Output, "\n" )
+print("Time Complexity is: O(n log k) and Space is O(k)\n" )
