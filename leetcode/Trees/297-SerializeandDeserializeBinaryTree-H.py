4eef354af82bedca7077c798f010adbd8696abfa
darshan <darshanradadiya29@gmail.com>
2024-05-18 23:56:20 -0700

    added iterative version for serialize and deserialize BT.

--- leetcode/Trees/297-SerializeandDeserializeBinaryTree-H.py
+++ leetcode/Trees/297-SerializeandDeserializeBinaryTree-H.py
@@ -1,3 +1,5 @@
+from typing import Optional
+from collections import deque
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, x):
@@ -49,3 +51,71 @@ class Codec:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+# ITERATIVE TRAVERSAL
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string.
+        """
+        if not root:
+            return ""
+        encodedStr = []
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node: 
+                encodedStr.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right) 
+            else:
+                encodedStr.append("N")
+        return ",".join(encodedStr)
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree.
+        """
+        if not data:
+            return None
+        data = data.split(",")
+        tree = deque(data)
+        root = TreeNode(int(tree.popleft()))
+        q = deque([root])
+        
+        while q:
+            node = q.popleft()
+            leftChild = tree.popleft()
+            if leftChild != "N":
+                node.left = TreeNode(int(leftChild))
+                q.append(node.left)
+            rightChild = tree.popleft()
+            if rightChild != "N":
+                node.right = TreeNode(int(rightChild))
+                q.append(node.right)
+            
+        return root
+
+        
+
+#        8
+#      /   \
+#     3     10
+#    / \    / \
+#   1   6   N 14
+#  / \ / \    / \
+# N  N 4  7  13  N
+
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
